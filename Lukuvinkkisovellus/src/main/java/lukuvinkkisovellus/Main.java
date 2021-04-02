9630f7ae3f194d5a8b9d3f1217fc2f1b83d8135a
nettivastaava <vaxandst@cubbli20-gold.local>
2021-04-02 13:58:00 +0300

    merge fixed

--- Lukuvinkkisovellus/src/main/java/lukuvinkkisovellus/Main.java
+++ Lukuvinkkisovellus/src/main/java/lukuvinkkisovellus/Main.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args) throws IOException {
-=======
     public static void main(String[] args) throws Exception {
->>>>>>> 4674a6049f219611ccc9360b406f9c7214bb0256
         Scanner reader = new Scanner(System.in);
 
         UI kayttis = new UI(reader);
