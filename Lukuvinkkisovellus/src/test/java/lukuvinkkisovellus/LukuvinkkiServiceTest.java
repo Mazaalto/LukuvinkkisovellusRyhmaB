05fd586ca52c01205708977b537f198d0135d065
Julia Bergman <julia.bergman@helsinki.fi>
2021-04-15 01:54:03 +0300

    Lisää yksikkötestejä LukuvinkkiServiceTest-luokkaan

--- Lukuvinkkisovellus/src/test/java/lukuvinkkisovellus/LukuvinkkiServiceTest.java
+++ Lukuvinkkisovellus/src/test/java/lukuvinkkisovellus/LukuvinkkiServiceTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author anttihalmetoja
+ * @author anttihalmetoja, jullebli
  */
 public class LukuvinkkiServiceTest {
     
@@ -46,4 +46,40 @@ public class LukuvinkkiServiceTest {
         assertTrue(lukuvinkkiService.listaaKaikki().toString().contains("Vinkin otsikko: otsikko2, vinkin linkki: testiurl2.com"));
     }
     
+    @Test
+    public void LukuvinkinLisaaminenKasvattaaLukuvinkkienMaaraa() throws Exception {
+        int maaraAluksi = lukuvinkkiService.getLukuvinkkienMaara();
+        lukuvinkkiService.lisaaLukuvinkki(new Lukuvinkki("otsikko1", "testiurl.com"));               
+        assertEquals(maaraAluksi + 1, lukuvinkkiDao.LukuvinkkienMaara());
+    }
+    
+    @Test
+    public void ListaaOtsikonPerusteellaPalauttaaOikeanLukumaaranLukuvinkkeja() throws Exception {
+        lukuvinkkiService.lisaaLukuvinkki(new Lukuvinkki("otsi", "testiurl.com"));
+        lukuvinkkiService.lisaaLukuvinkki(new Lukuvinkki("otsik", "testiurl2.com"));
+        lukuvinkkiService.lisaaLukuvinkki(new Lukuvinkki("otsikk", "testiur3.com"));
+        lukuvinkkiService.lisaaLukuvinkki(new Lukuvinkki("otsikko", "testiurl4.com"));
+        
+        assertEquals(4, lukuvinkkiService.listaaOtsikonPerusteella("otsi").size());
+    }
+    
+    @Test
+    public void ListaaOtsikonPerusteellaPalauttaaOikeanListanLukuvinkkeja() throws Exception {
+        Lukuvinkki a = new Lukuvinkki("otsi", "testiurl.com");
+        Lukuvinkki b = new Lukuvinkki("otsik", "testiurl2.com");
+        Lukuvinkki c = new Lukuvinkki("otsikk", "testiurl3.com");
+        Lukuvinkki d = new Lukuvinkki("otsikko", "testiurl4.com");
+        
+        List<Lukuvinkki> lukuvinkit = new ArrayList<>();
+        lukuvinkit.add(a);
+        lukuvinkit.add(b);
+        lukuvinkit.add(c);
+        lukuvinkit.add(d);
+        
+        lukuvinkkiService.lisaaLukuvinkki(a);
+        lukuvinkkiService.lisaaLukuvinkki(b);
+        lukuvinkkiService.lisaaLukuvinkki(c);
+        lukuvinkkiService.lisaaLukuvinkki(d);
+        assertArrayEquals(lukuvinkit.toArray(), lukuvinkkiService.listaaOtsikonPerusteella("otsi").toArray());
+    }
 }
